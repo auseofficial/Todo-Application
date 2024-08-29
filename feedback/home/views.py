429a182913df6fcd2fffe165ed835f5f2bff45a5
Akib Us Suny Eshan <eshan.r029@gmail.com>
2024-08-29 17:09:43 +0600

    updated form

--- feedback/home/views.py
+++ feedback/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . models import *
 
-# Create your views here.
+def index(request):
+    feedbacks = CustomerFeedback.objects.all()
+    return render(request,'surveys.html')
+
+
